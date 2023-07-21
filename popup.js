c44e685fd865ef77f57301c3d87a7ca5835ce8bb
rebroccoli <rwang1129@gmail.com>
2023-07-21 03:11:01 -0500

    random response works

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
-const generateResult = () => {
-  popup.html.getElementById("result").innerHTML = "hi";
+const button = document.getElementById("submitButton");
+button.onclick = function () {
+  generateResponse();
 };
+
+async function generateResponse() {
+  const responses = ["yes!", "maybe", "absolutely not!", "probably..."];
+  document.getElementById("result").innerHTML = responses[Math.floor(Math.random() * responses.length)];
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // makes button and listens for the click
+//   var button = document.getElementById("submitButton");
+
+//   button.addEventListener("click", function () {
+//     // randomly selects a response
+//   });
+// });
